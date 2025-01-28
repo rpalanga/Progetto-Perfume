c50443d421f7683c508116cd88088aeb3f3290e2
roberto-palanga <r.palanga94@gmail.com>
2025-01-28 17:59:41 +0100

    Inserimento View dashboard admin

--- perfume-backend/resources/views/admin/dashboard.blade.php
+++ perfume-backend/resources/views/admin/dashboard.blade.php
@@ -3,7 +3,8 @@
 @section('content')
 
 <div class="container">
-    <h1>CIao a tutti</h1>
+    <h1 class=" text-white-50">Ben venuto nella tua sezione</h1>
+    <p>Benvenuto</p>
 </div>
 
 @endsection
